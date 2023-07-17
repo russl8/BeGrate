8d58c917e2ee7a015b5007fd25e31596ad254a23
russl8 <russl8@my.yorku.ca>
2023-07-17 15:32:13 -0400

    home page post display

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,28 +1,47 @@
 import React, { useState, useEffect } from "react";
-
+import uniqid from "uniqid"
+import { NavLink } from "react-router-dom"
 export default function Home() {
-    const [currentText, setCurrentText] = useState("");
-    useEffect(() => {
-        fetch("http://localhost:3001/")
-          .then((res) => {
-            if (res.ok) {
-              return res.json(); // Parse the response body as JSON
-            } else {
-              throw new Error("Error fetching data.");
-            }
-          })
-          .then((jsonResponse) => {
-            setCurrentText(jsonResponse.message);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }, []);
-      
+  const [currentText, setCurrentText] = useState("");
+  const [allPosts, setAllPosts] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/")
+      .then((res) => {
+        if (res.ok) {
+          return res.json(); // Parse the response body as JSON
+        } else {
+          throw new Error("Error fetching data.");
+        }
+      })
+      .then((jsonResponse) => {
+        setAllPosts(jsonResponse.allPosts);
+        setCurrentText(jsonResponse.message);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h1>{currentText}</h1>
-        </div>
-    );
+
+  return (
+    <div>
+      <h1>{currentText}</h1>
+      {allPosts.map(post => {
+        return (
+          <div className="homepagePost">
+
+            <NavLink key={uniqid()} to={`/posts/${post._id}`}>
+              <h1>{post.title}</h1>
+              <NavLink to={`/account/${post.user._id}`}>
+                <h3>{post.user.username}</h3>
+
+              </NavLink>
+              <p>{post.content}</p>
+            </NavLink>
+          </div>
+
+        )
+      })}
+    </div>
+  );
 }
